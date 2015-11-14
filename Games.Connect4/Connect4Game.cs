43f94259e7c8f606826df94dce394bc33c36f695
mike wardle <nobbys_bigfoot@hotmail.com>
2015-11-14 10:17:47 +0000

    added logic to make game creation work

--- Games.Connect4/Connect4Game.cs
+++ Games.Connect4/Connect4Game.cs
@@ -1,21 +1,31 @@
-﻿using Games.Core;
+﻿using Games.Connect4.Players;
+using Games.Core;
+using Games.Core.Exceptions;
 
 namespace Games.Connect4
 {
-    public class Connect4Game :IGame
-    {
-		private int rows;
-		private int columns;
-		private IPlayer player1;
-		private IPlayer player2;
+	public class Connect4Game :IGame
+	{
+		public int Rows {get; private set;}
+		public int Columns { get; private set; }
+		public Connect4Player Player1 { get; private set; }
+		public Connect4Player Player2 { get; private set; }
 
 		public Connect4Game(int rows, int columns, IPlayer player1, IPlayer player2)
 		{
+			if (rows < 5 || rows > 9)
+				throw new InvalidGameConfigurationException();
+			if (columns < 5 || columns > 9)
+				throw new InvalidGameConfigurationException();
+			
 			// TODO: Complete member initialization
-			this.rows = rows;
-			this.columns = columns;
-			this.player1 = player1;
-			this.player2 = player2;
+			Rows = rows;
+			Columns = columns;
+			Player1 = player1 as Connect4Player;
+			Player2 = player2 as Connect4Player;
+
+			if (Player1 == null || Player2 == null)
+				throw new InvalidPlayerTypeException();
 		}
-    }
+	}
 }
